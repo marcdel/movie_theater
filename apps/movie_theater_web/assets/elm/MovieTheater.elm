b4658cfea71eba2df3690aa8e7dbca8cbcc7c508
Marc Delagrammatikas <delagrammatikas@gmail.com>
2017-04-05 19:57:10 -0700

    Add model and test data and display it in the view.

--- apps/movie_theater_web/assets/elm/MovieTheater.elm
+++ apps/movie_theater_web/assets/elm/MovieTheater.elm
@@ -1,11 +1,50 @@
 module MovieTheater exposing (..)
 
-import Html
+import Html exposing (ul, li, text)
+import Html.Attributes exposing (class)
 
 
 main =
-    view
+    view init
 
 
-view =
-    Html.text "Hello from view."
+
+-- MODEL
+
+
+init =
+    [ { seatNo = 1, occupied = False }
+    , { seatNo = 2, occupied = False }
+    , { seatNo = 3, occupied = False }
+    , { seatNo = 4, occupied = False }
+    , { seatNo = 5, occupied = False }
+    , { seatNo = 6, occupied = False }
+    , { seatNo = 7, occupied = False }
+    , { seatNo = 8, occupied = False }
+    , { seatNo = 9, occupied = False }
+    , { seatNo = 10, occupied = False }
+    , { seatNo = 11, occupied = False }
+    , { seatNo = 12, occupied = False }
+    ]
+
+
+type alias Seat =
+    { seatNo : Int
+    , occupied : Bool
+    }
+
+
+type alias Model =
+    List Seat
+
+
+
+-- VIEW
+
+
+view model =
+    ul [ class "seats" ] (List.map seatItem model)
+
+
+seatItem seat =
+    li [ class "seat available" ] [ text (toString seat.seatNo) ]
